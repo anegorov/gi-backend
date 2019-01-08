f2ae12d7a42f6efb84ada8a1ac1e6b4be9134524
andrey.egorov <andrey.egorov@taskdata.com>
2019-01-08 20:19:33 +0300

    first

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const Joi = require('joi');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const docs = [
+    {id:1, name:'doc1'},
+    {id:2, name:'doc2'},
+    {id:3, name:'doc3'}
+]
+
+app.get('/',(req,res) => {
+   res.send('App developed by Andrey Egorov.');
+});
+
+app.get('/api/docs',(req,res) => {
+   res.send(docs);
+});
+
+app.get('/api/docs/:id',(req,res) => {
+  const doc = docs.find(c => c.id === parseInt(req.params.id));
+  if(!doc) res.status(404).send('The document with the given id wasnt found');
+   res.send(doc);
+});
+
+const port = process.env.port || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
