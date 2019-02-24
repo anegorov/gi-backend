a1ef86f5224295a465aed1dce8a557870a513fe1
Andrey Egorov <a-n-egorov@yandex.ru>
2019-02-24 19:55:00 +0300

    Removed pug. Works fine without it.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const render = require('./routs/render');
-const pug = require('pug');
 const path = require('path');
 const mongoose = require('mongoose');
 const {Product, validate} = require('./models/product');
+const cloudinary = require('cloudinary');
+
+cloudinary.config({ 
+  cloud_name: 'dx8upsrgu', 
+  api_key: '468918147237958', 
+  api_secret: 'LcXS1GTR_vHusbTXAIliLX1N5gk' 
+});
 
 mongoose.connect('mongodb://giuser:giuser1234@ds024748.mlab.com:24748/guidein', { useNewUrlParser: true })
     .then(() => console.log('Connected to MongoDB...'))
@@ -17,26 +23,52 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/api/render', render);
 
-const texts = [
-  { title: 'Hey1', message: 'Hello there!1' },
-  { title: 'Hey2', message: 'Hello there!2' },
-  { title: 'Hey3', message: 'Hello there!3' }
-]
+function getImageUrl(imageName){
+  return cloudinary.image(imageName, { alt: "Image num one" });
+}
 
 app.get('/',(req,res) => {
-   res.send('App developed by Andrey Egorov.');
+   res.send('Img=' + getImageUrl("bed-gulliver/sborka1.jpg")); 
 });
+
+var cb1 = async function (req, res, next) {
+  const product = await Product
+  .find({link: req.params.link})
+  .select({images: 1});
+
+  if(!product) res.status(400).send(`There is no page with link [${req.params.id}]`); 
+
+  console.log('H:'+product.images);
+  next();
+}
 
 app.get('/render/:link', async function (req, res) {
 
   const product = await Product
     .find({link: req.params.link});
 
-  console.log(product);
+    let imagesList = product[0].images.reduce((accamulator, currVal) => {
+      return accamulator + getImageUrl(currVal);
+    },0);
+    imagesList = imagesList.substr(1,imagesList.lenght);
 
-  if(!product) res.status(400).send(`There is no page with link [${req.params.id}]`); 
-  
-  res.render('index', { page: product});
+    let html = `
+    <!DOCTYPE html>
+    <html>
+      <head><title>${product[0].sname}</title></head>
+      <body>
+        <h2>${product[0].lname}</h2>
+        <div>
+          ${imagesList}
+          <p>${product[0].description}</p>
+        </div>
+      </body>
+    </html>`;
+    
+    console.log(html);
+
+    res.send(html);
+    
 });
 
 const server = app.listen(process.env.PORT || 8080, function () {
