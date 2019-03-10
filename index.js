565918ca65f5f45894151b4d0992571b9f397e5f
andrey.egorov <andrey.egorov@taskdata.com>
2019-03-10 12:34:45 +0300

    Finat CORS

--- index.js
+++ index.js
@@ -28,12 +28,6 @@ mongoose.connect('mongodb://giuser:giuser1234@ds024748.mlab.com:24748/guidein', 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-// app.use((req, res, next) => {
-//   res.append('Access-Control-Allow-Origin', ['*']);
-//   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.append('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
 app.use(cors());
 app.use('/api/render', render);
 app.use('/api/users', users);
